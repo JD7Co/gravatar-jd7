20a11a153a35acb5050b3208a79337e697ca8b25
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-03-16 20:57:14 +0000

    Update gravatar.js

--- gravatar.js
+++ gravatar.js
@@ -1,1 +1,16 @@
 const crypto = require('crypto');
+
+function getGravatarHash(email) {
+
+  // 1. Убираем пробелы
+  // 2. Переводим в нижний регистр
+  email = email.trim().toLowerCase();
+
+  // 3. Создаём SHA256
+  const hash = crypto
+    .createHash('sha256')
+    .update(email, 'utf8')
+    .digest('hex');
+
+  return hash;
+}
