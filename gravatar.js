a6e9e90b65029b279f9b3339f1b06bbcef2dd961
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-03-16 21:01:57 +0000

    Update gravatar.js

--- gravatar.js
+++ gravatar.js
@@ -21,3 +21,7 @@ const hash = getGravatarHash(email);
 const avatarUrl = `https://www.gravatar.com/avatar/${hash}?s=200&d=identicon`;
 
 const profileUrl = `https://www.gravatar.com/${hash}.json`;
+
+console.log("Hash:", hash);
+console.log("Avatar:", avatarUrl);
+console.log("Profile:", profileUrl);
